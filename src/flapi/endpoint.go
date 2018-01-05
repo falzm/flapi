a24f35dedbc6472410a4bcfd6546fe7944acbde2
Marc Falzon <marc.falzon@d2-si.eu>
2018-01-05 16:00:16 +0100

    Implement endpoint delay probability

--- src/flapi/endpoint.go
+++ src/flapi/endpoint.go
@@ -9,24 +9,32 @@ import (
 )
 
 type endpoint struct {
-	method   string
-	route    string
-	response string
-	code     int
-	delay    time.Duration
+	method      string
+	route       string
+	response    string
+	code        int
+	delay       time.Duration
+	probability float64
 }
+
+var defaultProbability = 0.0
 
 func newEndpoint(method, route, response string, code int) *endpoint {
 	return &endpoint{
-		method:   method,
-		route:    route,
-		response: response,
-		code:     code,
+		method:      method,
+		route:       route,
+		response:    response,
+		code:        code,
+		probability: defaultProbability,
 	}
 }
 
 func (e *endpoint) handler(rw http.ResponseWriter, r *http.Request) {
-	time.Sleep(time.Duration(1+rand.Intn(10))*time.Millisecond + e.delay)
+	time.Sleep(time.Duration(1+rand.Intn(10)) * time.Millisecond)
+
+	if p := rand.Float64(); p > e.probability {
+		time.Sleep(e.delay)
+	}
 
 	rw.WriteHeader(e.code)
 
@@ -70,14 +78,29 @@ func handleDelay(rw http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		e.setDelay(time.Duration(delayValue) * time.Millisecond)
-		log.Info("delay for endpoint %s:%s adjusted to %s", e.method, e.route, e.delay)
+		probability := defaultProbability
+		if r.URL.Query().Get("probability") != "" {
+			probability, err = strconv.ParseFloat(r.URL.Query().Get("probability"), 64)
+			if err != nil {
+				http.Error(rw, "Invalid value for probability parameter", http.StatusBadRequest)
+				return
+			}
+		}
+
+		e.setDelay(time.Duration(delayValue)*time.Millisecond, probability)
+		log.Info("delay for endpoint %s:%s adjusted to %s with probability %.1f",
+			e.method,
+			e.route,
+			e.delay,
+			e.probability,
+		)
 
 		rw.WriteHeader(http.StatusNoContent)
 		return
 	}
 }
 
-func (e *endpoint) setDelay(delay time.Duration) {
+func (e *endpoint) setDelay(delay time.Duration, probability float64) {
 	e.delay = delay
+	e.probability = probability
 }
